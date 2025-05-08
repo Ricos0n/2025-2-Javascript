687f61f83f1bcb4829c4aa10fb37c2d6ce34306e
Ricos0n <ricardogutzfls@gmail.com>
2025-05-07 21:46:22 -0600

    entrega tareas

---
+++ semana1/clase1_tarea.js
@@ -0,0 +1,22 @@
+// Ejercicio: Crear una calculadora simple que realice operaciones básicas (suma, resta, multiplicación y división) entre dos números.
+
+const { ask } = require('../helpers/input');
+
+async function main() {
+  const num1 = Number(await ask('Ingresa el primer número: '));
+  const num2 = Number(await ask('Ingresa el segundo número: '));
+
+  const suma = num1 + num2;
+  const resta = num1 - num2;
+  const multiplicación = num1 * num2;
+  const division = num1 / num2;
+
+  console.log('Suma:', suma);
+  console.log('resta:', resta);
+  console.log('multiplicación:', multiplicación);
+  console.log('division:', division);
+  // TODO: Mostrar el resultado de la resta, multiplicación y división
+
+}
+
+main();
--- semana1/ejercicio1.js
+++ semana1/ejercicio1.js
@@ -1,11 +1,15 @@
-const { ask } = require('../helpers/input.js');
+// Clase 1: ¿Qué es programar? + Variables y Tipos de Datos
+
+const { ask } = require('../helpers/input');
 
 async function main() {
-  const name = await ask('¿Cuál es tu nombre?');
-  const age = Number(await ask('¿Cuál es tu edad?'));
+  const name = await ask("¿Cómo te llamas? ");
+  const age = Number(await ask("¿Cuántos años tienes? "));
+  const food = await ask("¿comida favorita?")
   // TODO : Agregar pregunta para comida favorita
 
-  console.log(`Hola ${name}, tienes ${age} años.`);
+  console.log("Hola " + name + ", tienes " + age + " años."+", tu comida fav es "+ food);
+  //console.log(`Tu comida favorita es ${comida}.`);
 }
 
 main();
